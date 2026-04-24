ee8bc8b0a8d11c1a8834dbceeb649d53a1831080
toyinsoladev <toyinsoladev@gmail.com>
2026-04-24 11:59:45 +0100

    Refactor initial items in packing list and update PackingList component to handle item deletion

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import logo from "./assets/logo.png";
 import { use, useState } from "react";
 
 const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Shirts", quantity: 5, packed: false },
-  { id: 4, description: "Pants", quantity: 3, packed: false },
-  { id: 5, description: "Toothbrush", quantity: 1, packed: false },
+  { id: 1, description: "Passport", quantity: 1, packed: false },
+  { id: 2, description: "Shirts", quantity: 5, packed: false },
+  { id: 3, description: "Pants", quantity: 3, packed: false },
+  { id: 4, description: "Toothbrush", quantity: 1, packed: false },
 ];
 
 export default function App() {
@@ -25,7 +24,7 @@ export default function App() {
       <Logo />
       <PackingListForm onAddItem={handleAddItem} />
       <hr />
-      <PackingList items={items} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
       <Stats />
       <Footer />
     </div>
@@ -93,17 +92,17 @@ function PackingListForm({ onAddItem }) {
   );
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <li key={item.id}>
+          <li key={item.id} onDeleteItem={onDeleteItem}>
             <input type="checkbox" />
             <span style={item.packed ? { textDecoration: "line-through" } : {}}>
               {item.quantity} {item.description}
             </span>
-            <button>❌</button>
+            <button onClick={() => onDeleteItem(item.id)}>❌</button>
           </li>
         ))}
       </ul>
